6f2e51d903f7a3389a48d5ddd6c0f1fbbc769428
Kgaugelo Bopape <kgaugelo20@gmail.com>
2017-02-10 12:08:47 +0200

    Registered pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+// App pages
 import { HomePage } from '../pages/home/home';
+import { LightPage } from '../pages/light/light';
+import { PoolPage } from '../pages/pool/pool';
+import { IrrigationPage } from '../pages/irrigation/irrigation';
+import { TabsPage } from '../pages/tabs/tabs';
+import { SettingsPage } from '../pages/settings/settings';
+import { HealthPage } from '../pages/health/health';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LightPage,
+    PoolPage,
+    IrrigationPage,
+    TabsPage,
+    SettingsPage,
+    HealthPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,8 +27,14 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LightPage,
+    PoolPage,
+    IrrigationPage,
+    TabsPage,
+    SettingsPage,
+    HealthPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }]
 })
-export class AppModule {}
+export class AppModule { }
